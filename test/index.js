d4b7f8d5c0b5cd85a02790bb3a8ab508e8f14b53
Antonín Bouchal <scorpio1337@Antonin-MacBook-Pro.local>
2018-08-11 15:47:15 +0200

    Test2

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 console.log("Node version: ", process.env.TRAVIS_NODE_VERSION);
 
-import './exportTypes'
-import './crossLoading'
-import './complexServices'
-import './predefinedServices'
-import './asyncServices'
+// import './exportTypes'
+// import './crossLoading'
+// import './complexServices'
+// import './predefinedServices'
+// import './asyncServices'
